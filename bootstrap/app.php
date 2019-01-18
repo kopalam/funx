96664bad915996de14b399181a4ffbe6f3e6be6c
kopa <1113288996@qq.com>
2019-01-18 21:10:15 +0800

    set artisan code:model

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -77,6 +77,7 @@ $app->singleton(
 | totally optional, so you are not required to uncomment this line.
 |
 */
+$app->register(biliboobrian\lumenAngularCodeGenerator\Provider\GeneratorServiceProvider::class);
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
